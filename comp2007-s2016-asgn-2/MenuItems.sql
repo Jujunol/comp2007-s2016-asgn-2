dbbb01faa11c4120344aeee2a3721752a054c141
Jujunol <jujunol@outlook.com>
2016-07-28 23:32:13 -0400

    Added Database SQL

---
+++ comp2007-s2016-asgn-2/MenuItems.sql
@@ -0,0 +1,11 @@
+﻿insert into MenuItems (Title, Price, ImageName, Description) values 
+("Salad", 3.99, "Salad.png", "With so many fresh vegetables, fruits, and proteins to choose from, you’ll be able to create your own tasty and healthy salad in a snap!"),
+("Soup & Bread", 3.99, "SoupAndBread.png", "Savour our selection of steaming soups, complemented by an array of freshly baked breads."),
+("Hot Entrées", 7.99, "HotEntrees.png", "Feast on a wide variety of freshly prepared entrées from our inspired signature items to traditional Chinese-Canadian favourites."),
+("Grilled Meat", 8.99, "GrilledMeat.png", "Enjoy the grill all year round as our talented Grill Attendants heat things up using an array of deliciously seasoned meats and vegetables."),
+("Sushi", 5.99, "Sushi.png", "Take your taste buds to Japan as our Sushi Chefs create sushi and maki rolls right before your eyes!"),
+("Pizza", 10.99, "Pizza.png", "Get ooey-gooey goodness with every bite of our classic oven-baked pizzas."),
+("Dessert", 6.99, "Dessert.png", "Satisfy your sweet-tooth with the perfect end to the perfect meal. Enjoy a piece of cake, a slice of pie, a crisp waffle… or try them all and more!"),
+("Cotton Candy", 6.99, "CottonCandy.png", "This melt-in-your-mouth treat tastes even better when it’s freshly made at our Cotton Candy stand!"),
+("Ice Cream Sundae & Frozen Yogurt", 6.99, "IceCream.png", "Build your own ice cream sundae or frozen yogurt bowl with our wide selection of flavours and delicious array of toppings."),
+("Fruit", 3.99, "Fruit.png", "Indulge in our fully-stocked fruit table featuring a wide selection of seasonal fresh fruit.");
