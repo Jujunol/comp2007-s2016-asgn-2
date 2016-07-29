9abe60710ff9959a322cf365d5f1108e27a31309
Jujunol <jujunol@outlook.com>
2016-07-28 23:31:17 -0400

    Initial Commit

---
+++ comp2007-s2016-asgn-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="comp2007_s2016_asgn_2.MvcApplication" Language="C#" %>
---
+++ comp2007-s2016-asgn-2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comp2007_s2016_asgn_2.Startup))]
+namespace comp2007_s2016_asgn_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
